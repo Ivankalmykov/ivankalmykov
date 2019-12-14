f14421fd9a82fad36ce533cfedca4a5f84a45a23
Ivankalmykov <Banu4@yandex.ru>
2019-12-14 18:13:28 +0400

    Методы

--- src/javacourse/homework2/Solution.java
+++ src/javacourse/homework2/Solution.java
@@ -3,7 +3,7 @@ package javacourse.homework2;
 public class Solution {
     public static void main(String[] args) {
         Notepad notepad = new Notepad();
-        for (int i =0 ; i<20; i++){
+        for (int i =0 ; i<40; i++){
             notepad.add("number "+i);
         }
         notepad.print();
