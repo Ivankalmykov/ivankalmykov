b7990cd682aa1556c3da6dff10abb262eddec2b9
Ivankalmykov <Banu4@yandex.ru>
2019-12-21 10:20:20 +0400

    Работа со строками

--- src/javacourse/homework3/Workwithstring.java
+++ src/javacourse/homework3/Workwithstring.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Workwithstring {
     public static void main(String[] args) {
         /*length();
-        whoIsLonger();
+        equalsIgnoreCase();
         internmethod();
         toCharArray();
         byteArray();
@@ -22,28 +22,25 @@ public class Workwithstring {
 
     }
 
-    private static void length() {
+    private static void length() {    //Получить длину строки
         String str = "Мама мыла раму";
         System.out.println(str.length());
     }
 
-    private static void whoIsLonger() {
+    private static void equalsIgnoreCase() {  // Сравнить 2 строки без учета регистра
         String str = "Мама мыла раму";
-        String str2 = "Папа мыл полы";
-        if (str.length() > str2.length()) {
-            System.out.println("Строка Str длинее строки Str2");
-        } else {
-            System.out.println("Строка Str2 длинее строки Str");
-        }
+        String str2 = "мама мыла раму";
+        boolean result = str.equalsIgnoreCase(str2);
+        System.out.println(result);
     }
 
-    private static void internmethod(){
+    private static void internmethod(){  // Создать новую строку с помощью конструктора и занести ее в пул литералов
         String a = new String("Галоша");
         String b = a.intern();
         System.out.println(b);
     }
 
-    private static void toCharArray(){
+    private static void toCharArray(){ // Получить из строки массив символов
         String str4 = "Галоша";
         char[] ch = str4.toCharArray();
         for (char a : ch) {
@@ -51,16 +48,16 @@ public class Workwithstring {
         }
 
     }
-    private static void byteArray(){
+    private static void byteArray(){ // Получить из строки массив байтов
         String str5 = "Goal";
         byte[] byteArray = str5.getBytes();
         System.out.println(Arrays.toString(byteArray));
     }
-    private static void upperCase(){
+    private static void upperCase(){ // Привести строку к верхнему регистру
         String st = "epam";
         System.out.println(st.toUpperCase());
     }
-    private static void firstA(){
+    private static void firstA(){ // Найти первую позицию символа "а" в строке
         String st = "Panamera";
         int indexA = st.indexOf("a");
         if(indexA == - 1) {
@@ -70,43 +67,43 @@ public class Workwithstring {
         }
     }
 
-    private static void lastA(){
+    private static void lastA(){ // Найти последнюю позицию символа "а" в строке
         String st = "Panamera";
         System.out.println(st.lastIndexOf("a"));
     }
 
-    private static void sun(){
+    private static void sun(){ // Проверить содержит-ли строка слово "Sun"
         String str = "hot summer sun";
         System.out.println(str.contains("sun"));
     }
 
-    private static void oracle(){
+    private static void oracle(){ // Проверить оканчивается-ли строка на "Oracle"
         String str = "It's the world you see on the plaque in the room of the Oracle";
         System.out.println(str.endsWith("Oracle"));
     }
-    private static void startWithJava(){
+    private static void startWithJava(){ //Проверить начинается-ли строка на "Java"
         String str = "Java - лучший язык программирвоания ?";
         System.out.println(str.startsWith("Java"));
     }
 
-    private static void swapAtoO(){
+    private static void swapAtoO(){ //Заменить все символы "а" в строке на символ "о"
         String str = "lamborghini".replaceAll("a", "o");
         System.out.println(str);
     }
-    private static void substring(){
+    private static void substring(){ //Получить подстроку с 44 символа по 90 символ
         String str = "С точки зрения программирования, инкапсуляция– это «сокрытие реализации». Мне нравится такое определение. ";
         System.out.println(str.substring(44, 90));
     }
-    private static void stringMassive(){
+    private static void stringMassive(){ //Разбить строку по символу пробел (т.е. чтобы каждое слово было отдельным элементом массива)
         String str = "Мама мыла раму";
         String[] st = str.split(" ");
         System.out.println(Arrays.toString(st));
 
     }
 
-    private static void reverse(){
+    private static void reverse(){ //Поменять последовательность символов в строке на обратную.
         String str = "abcdefg";
-        String reversedString = new StringBuffer(str).reverse().toString();
+        String reversedString = new StringBuilder(str).reverse().toString();
         System.out.println(reversedString);
     }
 
